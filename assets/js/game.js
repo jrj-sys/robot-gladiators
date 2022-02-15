4850d56fc92f1df3f0c9073873564e91d837163c
jrj-sys <jamesjamel99@gmail.com>
2022-02-15 03:44:49 -0600

    starter robot round created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,31 @@
-// this creates a function named "fight"
-
-function fight() {
-    window.alert("The fight has begun!");
-}
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-// fight();
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    //Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+    // subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the `enemyHealth` value
+    enemyHealth = (enemyHealth - playerAttack); 
+
+    // log a resulting message to the console so we know that it worked 
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // subtract the value of `enemyAttack` from the value of `playerHealth` and use that to update the value in `playerHealth`
+    playerHealth = (playerHealth - enemyAttack);
+    
+    // log a resulting message to the console so we know that it worked
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    
+
+fight();
+
+
 
 
